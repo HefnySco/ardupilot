8ea974ddbb20f393a9fcf8162eeadde7f8c177e9
HefnySco <mohammad.hefny@gmail.com>
2022-02-25 19:12:57 +0200

    fix: soc_code

--- libraries/AP_HAL_Linux/Util_RPI.cpp
+++ libraries/AP_HAL_Linux/Util_RPI.cpp
@@ -35,60 +35,60 @@ int UtilRPI::_check_rpi_version_by_rev()
     const unsigned int MAX_SIZE_LINE = 50;
     typedef struct  {
         const char* revision;
-        int cpu_code;
+        int soc_code;
     } rpi_revision_table;
 
     // @see https://elinux.org/RPi_HardwareHistory
     // @see https://github.com/raspberrypi/documentation/blob/develop/documentation/asciidoc/computers/raspberry-pi/revision-codes.adoc
     rpi_revision_table const rpi_revision[]= {
-        { revision:"2", cpu_code:1},   // 1 Model B
-        { revision:"3", cpu_code:1},   // 1 Model B
-        { revision:"4", cpu_code:1},   // 1 Model B
-        { revision:"5", cpu_code:1},   // 1 Model B
-        { revision:"6", cpu_code:1},   // 1 Model B
-        { revision:"7", cpu_code:1},   // 1 Model A
-        { revision:"8", cpu_code:1},   // 1 Model A
-        { revision:"9", cpu_code:1},   // 1 Model A
-        { revision:"10", cpu_code:1},   // 1 Model B+
-        { revision:"11", cpu_code:1},   // 1 Model Compute Module 1	
-        { revision:"12", cpu_code:1},   // 1 Model A+
-        { revision:"13", cpu_code:1},   // 1 Model B+
-        { revision:"14", cpu_code:1},   // 1 Model Compute Module 1
-        { revision:"15", cpu_code:1},   // 1 Model A+
+        { revision:"2", soc_code:1},   // 1 Model B
+        { revision:"3", soc_code:1},   // 1 Model B
+        { revision:"4", soc_code:1},   // 1 Model B
+        { revision:"5", soc_code:1},   // 1 Model B
+        { revision:"6", soc_code:1},   // 1 Model B
+        { revision:"7", soc_code:1},   // 1 Model A
+        { revision:"8", soc_code:1},   // 1 Model A
+        { revision:"9", soc_code:1},   // 1 Model A
+        { revision:"10", soc_code:1},   // 1 Model B+
+        { revision:"11", soc_code:1},   // 1 Model Compute Module 1	
+        { revision:"12", soc_code:1},   // 1 Model A+
+        { revision:"13", soc_code:1},   // 1 Model B+
+        { revision:"14", soc_code:1},   // 1 Model Compute Module 1
+        { revision:"15", soc_code:1},   // 1 Model A+
         
-        { revision:"a01040", cpu_code:2},   // 2 Model B	1.0
-        { revision:"a01041", cpu_code:2},   // 2 Model B	1.1
-        { revision:"a21041", cpu_code:2},   // 2 Model B	1.1
-        { revision:"a22042", cpu_code:2},   // 2 Model B (with BCM2837)  1.2	 
+        { revision:"a01040", soc_code:2},   // 2 Model B	1.0
+        { revision:"a01041", soc_code:2},   // 2 Model B	1.1
+        { revision:"a21041", soc_code:2},   // 2 Model B	1.1
+        { revision:"a22042", soc_code:2},   // 2 Model B (with BCM2837)  1.2	 
         
-        { revision:"900092", cpu_code:1},   // Zero 1.2	
-        { revision:"900093", cpu_code:1},   // Zero 1.3	
-        { revision:"920093", cpu_code:1},   // Zero 1.3	
-        { revision:"9000c1", cpu_code:1},   // Zero W 1.1
+        { revision:"900092", soc_code:1},   // Zero 1.2	
+        { revision:"900093", soc_code:1},   // Zero 1.3	
+        { revision:"920093", soc_code:1},   // Zero 1.3	
+        { revision:"9000c1", soc_code:1},   // Zero W 1.1
         
-        { revision:"a02082", cpu_code:3},   // 3 Model B
-        { revision:"a020a0", cpu_code:3},   // Compute Module 3 (and CM3 Lite)
-        { revision:"a22082", cpu_code:3},   // 3 Model B
-        { revision:"a32082", cpu_code:3},   // 3 Model B
-        { revision:"a020d3", cpu_code:3},   // 3 Model B+
-        { revision:"9020e0", cpu_code:3},   // 3 Model A+
-        { revision:"a02100", cpu_code:3},   // Compute Module 3+
+        { revision:"a02082", soc_code:3},   // 3 Model B
+        { revision:"a020a0", soc_code:3},   // Compute Module 3 (and CM3 Lite)
+        { revision:"a22082", soc_code:3},   // 3 Model B
+        { revision:"a32082", soc_code:3},   // 3 Model B
+        { revision:"a020d3", soc_code:3},   // 3 Model B+
+        { revision:"9020e0", soc_code:3},   // 3 Model A+
+        { revision:"a02100", soc_code:3},   // Compute Module 3+
 
-        { revision:"a03111", cpu_code:4},   // 4 Model B
-        { revision:"b03111", cpu_code:4},   // 4 Model B
-        { revision:"b03112", cpu_code:4},   // 4 Model B
-        { revision:"b03114", cpu_code:4},   // 4 Model B
-        { revision:"c03111", cpu_code:4},   // 4 Model B
-        { revision:"c03112", cpu_code:4},   // 4 Model B
-        { revision:"c03114", cpu_code:4},   // 4 Model B
-        { revision:"d03114", cpu_code:4},   // 4 Model B
+        { revision:"a03111", soc_code:4},   // 4 Model B
+        { revision:"b03111", soc_code:4},   // 4 Model B
+        { revision:"b03112", soc_code:4},   // 4 Model B
+        { revision:"b03114", soc_code:4},   // 4 Model B
+        { revision:"c03111", soc_code:4},   // 4 Model B
+        { revision:"c03112", soc_code:4},   // 4 Model B
+        { revision:"c03114", soc_code:4},   // 4 Model B
+        { revision:"d03114", soc_code:4},   // 4 Model B
         
-        { revision:"a03140", cpu_code:4},   // Computer Module 4 1GB
-        { revision:"b03140", cpu_code:4},   // Computer Module 4 2GB
-        { revision:"c03140", cpu_code:4},   // Computer Module 4 4GB
-        { revision:"d03140", cpu_code:4},   // Computer Module 4 8GB
+        { revision:"a03140", soc_code:4},   // Computer Module 4 1GB
+        { revision:"b03140", soc_code:4},   // Computer Module 4 2GB
+        { revision:"c03140", soc_code:4},   // Computer Module 4 4GB
+        { revision:"d03140", soc_code:4},   // Computer Module 4 8GB
 
-        { revision:"902120", cpu_code:2}    // Zero 2 W
+        { revision:"902120", soc_code:2}    // Zero 2 W
     };
 
     // assume 2 if unknown
@@ -125,8 +125,8 @@ int UtilRPI::_check_rpi_version_by_rev()
         const unsigned int count = sizeof(rpi_revision)/ sizeof(rpi_revision_table);
         for (uint32_t i=0;i< count;++i) {
             if (strcmp(rpi_revision[i].revision, pch) == 0) {
-                printf ("Revision %s (intern: %d)\n", rpi_revision[i].revision, rpi_revision[i].cpu_code);
-                _rpi_version = rpi_revision[i].cpu_code;
+                printf ("Revision %s (intern: %d)\n", rpi_revision[i].revision, rpi_revision[i].soc_code);
+                _rpi_version = rpi_revision[i].soc_code;
                 break;
             }
         }
