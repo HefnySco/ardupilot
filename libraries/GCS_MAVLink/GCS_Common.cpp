2b5e44dfb4be8b0e3b4df27ba8844f5b3c299ddd
bugobliterator <siddharthbharatpurohit@gmail.com>
2022-05-31 11:24:59 +0900

    GCS_MAVLink: forward gimbal-device-information to AP_Mount
    Co-authored-by: Randy Mackay <rmackay9@yahoo.com>
    
    also forward gimbal_device_attitude_status

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -3633,6 +3633,8 @@ void GCS_MAVLINK::handle_common_message(const mavlink_message_t &msg)
         break;
 
     case MAVLINK_MSG_ID_GIMBAL_REPORT:
+    case MAVLINK_MSG_ID_GIMBAL_DEVICE_INFORMATION:
+    case MAVLINK_MSG_ID_GIMBAL_DEVICE_ATTITUDE_STATUS:
         handle_mount_message(msg);
         break;
 
