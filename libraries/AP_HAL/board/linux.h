7afa8b569a002e93d35079f1a3c25b4c0f2cd158
Mohammad Hefny <mohammad.hefny@gmail.com>
2022-06-13 04:41:38 +0200

    adding no baro

--- libraries/AP_HAL/board/linux.h
+++ libraries/AP_HAL/board/linux.h
@@ -308,11 +308,14 @@
         #define HAL_PARAM_DEFAULTS_PATH "/home/pi/ardupilot.parm"
 
         #define HAL_INS_PROBE_LIST PROBE_IMU_SPI(Invensense, "mpu9250", ROTATION_NONE)
-        #define HAL_MAG_PROBE_LIST PROBE_MAG_IMU(AK8963, mpu9250, 0, ROTATION_NONE)
-        #define HAL_BARO_PROBE_LIST PROBE_BARO_I2C(BMP085, 1, 0x77) 
+        //#define HAL_MAG_PROBE_LIST PROBE_MAG_IMU(AK8963, mpu9250, 0, ROTATION_NONE)
+        //#define HAL_BARO_PROBE_LIST PROBE_BARO_I2C(BMP085, 1, 0x77) 
         //#define HAL_MAG_PROBE_LIST PROBE_MAG_I2C(QMC5883L, 1, 0x0d,true ,  ROTATION_NONE)
 
-        #define HAL_PROBE_EXTERNAL_I2C_COMPASSES
+        #define ALLOW_ARM_NO_COMPASS
+        #define HAL_DISABLE_I2C_MAGS_BY_DEFAULT
+        #define HAL_BARO_ALLOW_INIT_NO_BARO
+        
     #endif
 
     
